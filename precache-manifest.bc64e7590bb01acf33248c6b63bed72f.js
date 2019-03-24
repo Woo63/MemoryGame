6b396aae32f18db0d71756380b6989e3f40a8e9a
Woo63 <55555r@list.ru>
2019-03-24 21:13:47 +0400

    Updates

---
+++ precache-manifest.bc64e7590bb01acf33248c6b63bed72f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d7d6dc6aeddc0d71059",
+    "url": "/MemoryGame/static/css/main.1ace4986.chunk.css"
+  },
+  {
+    "revision": "2d7d6dc6aeddc0d71059",
+    "url": "/MemoryGame/static/js/main.cc8fdd25.chunk.js"
+  },
+  {
+    "revision": "4c3bd79ed0e125202499",
+    "url": "/MemoryGame/static/js/runtime~main.d54f135b.js"
+  },
+  {
+    "revision": "fd95bb7216e29ee403a4",
+    "url": "/MemoryGame/static/js/2.4b74be8b.chunk.js"
+  },
+  {
+    "revision": "32414e2283755a106c35868a62508f17",
+    "url": "/MemoryGame/index.html"
+  }
+];
